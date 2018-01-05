1c5b01e507121d3807dcbc49ce15be7ba319fefd
南 京兵 <k_minami@waku-2.com>
2018-01-05 15:43:42 +0900

    Initial Commit

---
+++ Peephole/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peephole
+//
+//  Created by 南　京兵 on 2018/01/05.
+//  Copyright © 2018年 kyohei.minami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
